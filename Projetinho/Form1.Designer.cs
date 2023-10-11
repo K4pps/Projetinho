dcfa29a5d646d6ec5cd294e0d71a64313dc18ff2
K4pps <j.kapps@aluno.ifsp.edu.br>
2023-10-11 11:39:48 -0300

    Adicionar arquivos de projeto.

---
+++ Projetinho/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tabelao = new System.Windows.Forms.ListView();
+            this.textsenha = new System.Windows.Forms.TextBox();
+            this.textemail = new System.Windows.Forms.TextBox();
+            this.textname = new System.Windows.Forms.TextBox();
+            this.lbnome = new System.Windows.Forms.Label();
+            this.lbemail = new System.Windows.Forms.Label();
+            this.lbtele = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txttele = new System.Windows.Forms.MaskedTextBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btncad = new System.Windows.Forms.Button();
+            this.btnedit = new System.Windows.Forms.Button();
+            this.btnapagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Tabelao
+            // 
+            this.Tabelao.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.Tabelao.HideSelection = false;
+            this.Tabelao.Location = new System.Drawing.Point(394, 533);
+            this.Tabelao.Name = "Tabelao";
+            this.Tabelao.Size = new System.Drawing.Size(697, 151);
+            this.Tabelao.TabIndex = 0;
+            this.Tabelao.UseCompatibleStateImageBehavior = false;
+            this.Tabelao.View = System.Windows.Forms.View.Details;
+            // 
+            // textsenha
+            // 
+            this.textsenha.Location = new System.Drawing.Point(12, 602);
+            this.textsenha.Name = "textsenha";
+            this.textsenha.Size = new System.Drawing.Size(248, 20);
+            this.textsenha.TabIndex = 1;
+            // 
+            // textemail
+            // 
+            this.textemail.Location = new System.Drawing.Point(12, 495);
+            this.textemail.Name = "textemail";
+            this.textemail.Size = new System.Drawing.Size(248, 20);
+            this.textemail.TabIndex = 2;
+            this.textemail.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textname
+            // 
+            this.textname.Location = new System.Drawing.Point(12, 445);
+            this.textname.Name = "textname";
+            this.textname.Size = new System.Drawing.Size(248, 20);
+            this.textname.TabIndex = 4;
+            this.textname.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // lbnome
+            // 
+            this.lbnome.AutoSize = true;
+            this.lbnome.BackColor = System.Drawing.Color.Transparent;
+            this.lbnome.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbnome.Location = new System.Drawing.Point(12, 429);
+            this.lbnome.Name = "lbnome";
+            this.lbnome.Size = new System.Drawing.Size(42, 14);
+            this.lbnome.TabIndex = 5;
+            this.lbnome.Text = "Nome:";
+            // 
+            // lbemail
+            // 
+            this.lbemail.AutoSize = true;
+            this.lbemail.BackColor = System.Drawing.Color.Transparent;
+            this.lbemail.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbemail.Location = new System.Drawing.Point(12, 479);
+            this.lbemail.Name = "lbemail";
+            this.lbemail.Size = new System.Drawing.Size(39, 14);
+            this.lbemail.TabIndex = 6;
+            this.lbemail.Text = "Email:";
+            // 
+            // lbtele
+            // 
+            this.lbtele.AutoSize = true;
+            this.lbtele.BackColor = System.Drawing.Color.Transparent;
+            this.lbtele.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtele.Location = new System.Drawing.Point(12, 533);
+            this.lbtele.Name = "lbtele";
+            this.lbtele.Size = new System.Drawing.Size(55, 14);
+            this.lbtele.TabIndex = 7;
+            this.lbtele.Text = "Telefone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 586);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 14);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Senha:";
+            // 
+            // txttele
+            // 
+            this.txttele.Location = new System.Drawing.Point(12, 550);
+            this.txttele.Mask = "(00) 00000-0000";
+            this.txttele.Name = "txttele";
+            this.txttele.Size = new System.Drawing.Size(248, 20);
+            this.txttele.TabIndex = 9;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nome";
+            this.columnHeader1.Width = 140;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Email";
+            this.columnHeader2.Width = 140;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Telefone";
+            this.columnHeader3.Width = 140;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Senha";
+            this.columnHeader4.Width = 140;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "ID";
+            this.columnHeader5.Width = 180;
+            // 
+            // btncad
+            // 
+            this.btncad.Location = new System.Drawing.Point(12, 628);
+            this.btncad.Name = "btncad";
+            this.btncad.Size = new System.Drawing.Size(75, 23);
+            this.btncad.TabIndex = 10;
+            this.btncad.Text = "Entrar";
+            this.btncad.UseVisualStyleBackColor = true;
+            this.btncad.Click += new System.EventHandler(this.btncad_Click);
+            // 
+            // btnedit
+            // 
+            this.btnedit.Location = new System.Drawing.Point(93, 628);
+            this.btnedit.Name = "btnedit";
+            this.btnedit.Size = new System.Drawing.Size(75, 23);
+            this.btnedit.TabIndex = 11;
+            this.btnedit.Text = "Editar";
+            this.btnedit.UseVisualStyleBackColor = true;
+            this.btnedit.Click += new System.EventHandler(this.btnedit_Click);
+            // 
+            // btnapagar
+            // 
+            this.btnapagar.Location = new System.Drawing.Point(174, 628);
+            this.btnapagar.Name = "btnapagar";
+            this.btnapagar.Size = new System.Drawing.Size(75, 23);
+            this.btnapagar.TabIndex = 12;
+            this.btnapagar.Text = "Apagar";
+            this.btnapagar.UseVisualStyleBackColor = true;
+            this.btnapagar.Click += new System.EventHandler(this.btnapagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Projetinho.Properties.Resources.Olá__sou_o_Carlos_Gama___1_;
+            this.ClientSize = new System.Drawing.Size(1088, 682);
+            this.Controls.Add(this.btnapagar);
+            this.Controls.Add(this.btnedit);
+            this.Controls.Add(this.btncad);
+            this.Controls.Add(this.txttele);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbtele);
+            this.Controls.Add(this.lbemail);
+            this.Controls.Add(this.lbnome);
+            this.Controls.Add(this.textname);
+            this.Controls.Add(this.textemail);
+            this.Controls.Add(this.textsenha);
+            this.Controls.Add(this.Tabelao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView Tabelao;
+        private System.Windows.Forms.TextBox textsenha;
+        private System.Windows.Forms.TextBox textemail;
+        private System.Windows.Forms.TextBox textname;
+        private System.Windows.Forms.Label lbnome;
+        private System.Windows.Forms.Label lbemail;
+        private System.Windows.Forms.Label lbtele;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox txttele;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btncad;
+        private System.Windows.Forms.Button btnedit;
+        private System.Windows.Forms.Button btnapagar;
+    }
+}
+
---
+++ Projetinho/Form1.cs
@@ -0,0 +1,313 @@
+﻿using site;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Projeto
+{
+    public partial class Form1 : Form
+    {
+        private int Id;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateListView()
+        {
+            Tabelao.Items.Clear();
+
+            Connection conn = new Connection();
+            SqlCommand sqlCom = new SqlCommand();
+
+            sqlCom.Connection = conn.ReturnConnection();
+            sqlCom.CommandText = "SELECT * FROM KAPPS1";
+
+            try
+            {
+                SqlDataReader dr = sqlCom.ExecuteReader();
+
+                //Enquanto for possível continuar a leitura das linhas que foram retornadas na consulta, execute.
+                while (dr.Read())
+                {
+                    int id = (int)dr["ID"];
+                    string email = (string)dr["Email"];
+                    string senha = (string)dr["Senha"];
+                    string telefone = (string)dr["Telefone"];
+                    string nome = (string)dr["Nome"];
+
+                    ListViewItem lv = new ListViewItem(id.ToString());
+                    lv.SubItems.Add(nome);
+                    lv.SubItems.Add(email);
+                    lv.SubItems.Add(telefone);
+                    lv.SubItems.Add(senha);
+                    Tabelao.Items.Add(lv);
+
+                }
+                dr.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+
+        private bool ValidarEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateListView();
+        }
+
+        private void lbnome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private bool IsValidPhoneNumber(string telefone)
+        //{
+        //    // Define a expressão regular para validar um número de telefone no formato (99) 99999-9999
+        //    string pattern = @"^\(\d{2}\)\d{5}-\d{4}$";
+
+        //    // Cria um objeto Regex com a expressão regular
+        //    Regex regex = new Regex(pattern);
+
+        //    // Usa o método Match para verificar se o telefone corresponde ao padrão
+        //    return regex.IsMatch(telefone);
+        //}
+
+
+        private void lbtelefone_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void email_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Tabelao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"UPDATE KAPPS1 SET Email = @Email, Senha = @Senha, Cpf = @Cpf, Telefone = @Telefone, Nome = @Nome, WHERE ID = @ID";
+
+            sqlCommand.Parameters.AddWithValue("@Email", textemail.Text);
+            sqlCommand.Parameters.AddWithValue("@Senha", textsenha.Text);
+            sqlCommand.Parameters.AddWithValue("@Telefone", textemail.Text);
+            sqlCommand.Parameters.AddWithValue("@Nome", textname.Text);
+            sqlCommand.ExecuteNonQuery();
+        }
+
+        private void Tabelao_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index;
+
+            index = Tabelao.FocusedItem.Index;
+
+            Id = int.Parse(Tabelao.Items[index].SubItems[0].Text);
+
+            textname.Text = Tabelao.Items[index].SubItems[1].Text;
+
+            textemail.Text = Tabelao.Items[index].SubItems[2].Text;
+
+            textemail.Text = Tabelao.Items[index].SubItems[3].Text;
+
+            textsenha.Text = Tabelao.Items[index].SubItems[4].Text;
+
+
+        }
+
+        
+        private void LtvStudent_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index;
+            index = Tabelao.FocusedItem.Index;
+            Id = int.Parse(Tabelao.Items[index].SubItems[0].Text);
+            textname.Text = Tabelao.Items[index].SubItems[1].Text;
+            textemail.Text = Tabelao.Items[index].SubItems[2].Text;
+            textemail.Text = Tabelao.Items[index].SubItems[3].Text;
+            textsenha.Text = Tabelao.Items[index].SubItems[5].Text;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Código para excluir
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"DELETE FROM KAPPS1 WHERE Id = @id";
+            sqlCommand.Parameters.AddWithValue("@id", Id);
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao excluir usuário no banco.\n" + err.Message);
+            }
+            finally
+            {
+                connection.CloseConnection();
+            }
+
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncad_Click(object sender, EventArgs e)
+        {
+            string name = textname.Text, email = textemail.Text, telefone = txttele.Text;
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            {
+                sqlCommand.Connection = connection.ReturnConnection();
+                sqlCommand.CommandText = @"INSERT INTO KAPPS1 VALUES (@Email, @Senha, @Cpf, @Telefone, @Nome)";
+
+                sqlCommand.Parameters.AddWithValue("@Email", textemail.Text);
+                sqlCommand.Parameters.AddWithValue("@Senha", textsenha.Text);
+                sqlCommand.Parameters.AddWithValue("@Telefone", txttele.Text);
+                sqlCommand.Parameters.AddWithValue("@Nome", textname.Text);
+                sqlCommand.ExecuteNonQuery();
+            };
+
+
+            MessageBox.Show("Cadastro realizado com sucesso!");
+
+
+            //if (IsValidEmail(email) && ValidarCPF(cpf))
+            //{
+            //    MessageBox.Show("Email e CPF válidos!", "Validação Concluída", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Email ou CPF inválidos!", "Validação Falhou", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+        }
+
+        private void btnedit_Click(object sender, EventArgs e)
+        {
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"UPDATE KAPPS1 SET
+
+            Nome       = @nome, 
+            Email      = @email,
+            Cpf        = @cpf, 
+            Telefone   = @telefone, 
+            Senha      = @senha
+            where 
+            ID = @ID";
+
+            sqlCommand.Parameters.AddWithValue("@nome", textname.Text);
+            sqlCommand.Parameters.AddWithValue("@email", textemail.Text);
+            sqlCommand.Parameters.AddWithValue("@telefone", txttele.Text);
+            sqlCommand.Parameters.AddWithValue("@senha", textsenha.Text);
+            sqlCommand.Parameters.AddWithValue("@id", Id);
+
+            sqlCommand.ExecuteNonQuery();
+
+            MessageBox.Show("Editado com sucesso",
+                "AVISO",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            textname.Clear();
+            txttele.Clear();
+            textsenha.Clear();
+            textemail.Clear();
+
+
+            UpdateListView();
+        }
+
+        private void btnapagar_Click(object sender, EventArgs e)
+        {
+            //Código para excluir
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"DELETE FROM KAPPS1 WHERE Id = @id";
+            sqlCommand.Parameters.AddWithValue("@id", Id);
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao excluir usuário no banco.\n" + err.Message);
+            }
+            finally
+            {
+                connection.CloseConnection();
+            }
+        }
+    }
+}
